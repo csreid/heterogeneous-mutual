fb9fe527f50060a4779878ddc30a6703666ffe3b
Cameron Reid <csreid.cr@gmail.com>
2020-12-15 11:49:03 -0500

    Refactor configuration stuff

---
+++ experiment.py
@@ -0,0 +1,159 @@
+import gym
+from qlearner import QLearning
+from adp import CartPoleADP
+from mutual import MutHook, HeterogeneousMutualLearner
+import torch
+import pickle
+
+class Experiment:
+	def __init__(self, cfg):
+		self.cfg = cfg
+
+		self.use_target_q = cfg['useTargetQ']
+		self.q_target_lag = cfg['QTargetLag']
+		self.do_mutual = cfg['mutual']['ADPQ']
+		self.do_hetmut = cfg['mutual']['heterogeneous']
+		self.mutual_steps = cfg['mutual']['mutualSteps']
+
+		self.do_standard_q = cfg['standard']['Q']
+		self.do_standard_adp = cfg['standard']['ADP']
+
+		self.n_trials = cfg['trials']
+		self.steps = cfg['stepsPerTrial']
+		self.steps_per_eval = cfg['stepsPerEvaluation']
+		self.gamma = cfg['gamma']
+		self.adp_bins = cfg['adpBins']
+
+		self.env_name = cfg['envName']
+		if type(cfg['holdOutStates']) == bool and cfg['holdOutStates']:
+			self.hold_out_states = 100
+		elif type(cfg['holdOutStates']) == bool:
+			self.hold_out_states = None
+		else:
+			self.hold_out_states = cfg['holdOutStates']
+
+		if type(self.hold_out_states) == int:
+			self.held_states = self._get_held_states(self.hold_out_states)
+
+		self.loss_history = {}
+		self.results = []
+		self.names = None
+
+	def _do_step(self, agt, env, s):
+		a = agt.get_action(s)
+		sp, r, done, _ = env.step(a)
+		agt.handle_transition(s, a, r, sp, done)
+
+		if done:
+			sp = env.reset()
+
+		return sp
+
+	def save(self, fname=''):
+		if len(fname) > 0:
+			fname = '_' + fname
+
+		fname = f'./results/{time.time()}{fname}.pickle'
+		pickle.dump({
+			'names': self.names,
+			'results': self.results
+		}, open(fname, 'wb'))
+
+	def _get_held_states(self, n):
+		env = gym.make(self.env_name)
+		s_s = torch.zeros(n, 4)
+		for idx in range(n):
+			s = torch.tensor(env.reset())
+			s_s[idx] = s
+
+		return s_s
+
+	def _do_trial(self):
+		agts = self._build_agts()
+		envs = [gym.make(self.env_name).env for _ in agts]
+		eval_envs = [gym.make(self.env_name) for _ in agts]
+		s_s = [env.reset() for env in envs]
+		trial_evals = []
+
+		for agt in agts:
+			if agt.name not in self.loss_history:
+				self.loss_history[agt.name] = []
+
+		for step in range(self.steps):
+			for idx, (agt, env, s) in enumerate(zip(agts, envs, s_s)):
+				s_s[idx] = self._do_step(agt, env, s)
+
+			if (step % self.steps_per_eval) == 0:
+				evals = [agt.evaluate(eval_env, 20) for (agt, eval_env) in zip(agts, eval_envs)]
+				print(evals)
+				trial_evals.append(evals)
+
+		return trial_evals
+
+	def run(self):
+		for trial in range(self.n_trials):
+			trial_result = self._do_trial()
+			print(f'Trial {trial}')
+			self.results.append(trial_result)
+
+	def _build_agts(self):
+		agts = []
+		if self.do_mutual:
+			adp = CartPoleADP(
+				nbins=self.adp_bins,
+				gamma=self.gamma
+			)
+			qlrn = QLearning(
+				gamma=self.gamma,
+				target=self.use_target_q,
+				target_lag=self.q_target_lag,
+				mutual_steps=self.mutual_steps
+			)
+
+			hook = MutHook(adp)
+
+			qlrn.set_mutual_agents([adp])
+			qlrn.set_mutual_hook(hook)
+
+			adp.set_name('adp_sharing_tuples')
+			qlrn.set_name('q_mutual')
+
+			agts.append(adp)
+			agts.append(qlrn)
+
+		if self.do_standard_q:
+			qlrn = QLearning(
+				gamma=self.gamma,
+				target=self.use_target_q,
+				target_lag=self.q_target_lag
+			)
+
+			qlrn.set_name('q_standard')
+			agts.append(qlrn)
+
+		if self.do_standard_adp:
+			adp = CartPoleADP(
+				nbins=self.adp_bins,
+				gamma=self.gamma
+			)
+
+			adp.set_name('adp_standard')
+
+			agts.append(adp)
+
+		if self.do_hetmut:
+			hetmut = HeterogeneousMutualLearner(
+				mutual_steps=self.mutual_steps,
+				do_target_q=self.use_target_q,
+				q_target_lag=self.q_target_lag,
+				q_gamma=self.gamma,
+				adp_gamma=self.gamma,
+				adp_bins=self.adp_bins
+			)
+
+			hetmut.set_name('single_agent_mutual')
+			agts.append(hetmut)
+
+		if self.names is None:
+			self.names = [agt.name for agt in agts]
+		return agts
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from itertools import product
 import pickle
 import time
 from IPython import embed
+import json
 
 import numpy as np
 import torch
@@ -10,6 +11,7 @@ from memory import Memory
 from qlearner import QLearning
 from adp import CartPoleADP
 from mutual import MutHook, HeterogeneousMutualLearner
+from experiment import Experiment
 import matplotlib.pyplot as plt
 
 import gym
@@ -18,163 +20,12 @@ NBINS=9
 
 eval_env = gym.make('CartPole-v1')
 
-def do_step(agt, env, s):
-	a = agt.get_action(s)
-	sp, r, done, _ = env.step(a)
-
-	agt.handle_transition(s, a, r, sp, done)
-
-	if done:
-		done = False
-		sp = env.reset()
-
-	return sp
-
-def grab_states(env, n):
-	done = False
-	s = env.reset()
-	states = torch.zeros((n,) + env.observation_space.shape)
-
-	for idx in range(n):
-		states[idx] = torch.tensor(s)
-		s, r, done, _ = env.step(env.action_space.sample())
-
-		if done:
-			s = env.reset()
-			done = False
-
-	return states
-
-def do_experiment(
-	fname,
-	ml_type=None,
-	do_baseline=False,
-	do_mutual=False,
-	n_trials=50,
-	n_steps=5000,
-	mutual_steps=1000,
-	delta=0.01,
-	steps_per_eval=200
-):
-	trial_rs = []
-	all_mean_held_states = []
-	states = grab_states(gym.make('CartPole-v1'), 100)
-	all_loss_histories = {}
-
-	for trial in range(n_trials):
-		agts = []
-		rs = []
-		mean_held_states = []
-
-		adp = CartPoleADP(nbins=NBINS, gamma=gamma)
-		qlrn = QLearning(gamma=gamma)
-
-		if ml_type == 'share_tuples':
-			qlrn.set_mutual_agents([adp])
-			adp.set_name('adp_sharing_tuples')
-
-		if ml_type == 'share_qs':
-			hook = MutHook(adp)
-			qlrn.set_mutual_hook(hook)
-			qlrn.set_name('q_mutual')
-			qlrn.set_mutual_steps(mutual_steps)
-
-		if ml_type == 'both':
-			qlrn.set_mutual_agents([adp])
-			hook = MutHook(adp)
-			qlrn.set_mutual_hook(hook)
-			qlrn.set_name('q_mutual')
-			qlrn.set_mutual_steps(mutual_steps)
-			adp.set_name('adp_sharing_tuples')
-
-		if ml_type is not None:
-			agts.append(qlrn)
-			agts.append(adp)
-
-		if do_baseline == 'both':
-			qlrn_normal = QLearning(
-				gamma=gamma
-				target=True,
-				target_lag=100
-			)
-			adp_normal = CartPoleADP(nbins=NBINS, gamma=gamma)
-			qlrn_normal.set_name('q_standard')
-			adp_normal.set_name('adp_standard')
-			agts.append(qlrn_normal)
-			agts.append(adp_normal)
-
-		if do_baseline == 'q':
-			qlrn_normal = QLearning(
-				gamma=gamma
-				target=True,
-				target_lag=100
-			)
-			qlrn_normal.set_name('q_standard')
-			agts.append(qlrn_normal)
-
-		if do_baseline == 'adp':
-			qlrn_normal = CartPoleADP(nbins=NBINS, gamma=gamma)
-			qlrn_normal.set_name('adp_standard')
-			agts.append(qlrn_normal)
-
-		if do_mutual:
-			mutual = HeterogeneousMutualLearner(
-				mutual_steps=mutual_steps,
-				do_target_q = True,
-				q_target_lag=100,
-				q_gamma=gamma,
-				adp_gamma=gamma,
-				adp_bins=NBINS
-			)
-			mutual.set_name('single_agent_mutual')
-			agts.append(mutual)
-
-		envs = [gym.make('CartPole-v1').env for _ in agts]
-		s_s = [env.reset() for env in envs]
-		for agt in agts:
-			if agt.name not in all_loss_histories:
-				all_loss_histories[agt.name] = []
-
-		for step in range(n_steps):
-			for idx, (agt, env, s) in enumerate(zip(agts, envs, s_s)):
-				s_s[idx] = do_step(agt, env, s)
-
-			if (step % steps_per_eval) == 0:
-				evls = [agt.evaluate(eval_env, 20) for agt in agts]
-				rs.append(evls)
-
-			if (step % 10) == 0:
-				mean_held_states.append([torch.mean(agt.Q(states)) for agt in agts if agt.name in ['q_mutual', 'q_standard']])
-				evl_str = ' | '.join([f'{a.name}: {e}' for (a, e) in zip(agts, evls)])
-				print(f'Trial {trial}\t\tStep {step}  \t\tEvals {evl_str}')
-
-		trial_rs.append(rs)
-		all_mean_held_states.append(mean_held_states)
-		q_learning_agts = [agt for agt in agts if type(agt) == QLearning]
-		for agt in q_learning_agts:
-			all_loss_histories[agt.name].append(agt._loss_history)
-			print(len(all_loss_histories[agt.name]))
-
-	fname = f'./results/{time.time()}_{fname}.pickle'
-	pickle.dump({
-		'names': [agt.name for agt in agts],
-		'results': trial_rs,
-		'held_states': all_mean_held_states,
-		'losses': all_loss_histories
-	}, open(fname, 'wb'))
-
-	embed()
+def parse_config(fname):
+	data = json.load(open(fname, 'r'))
+	return data
 
 if __name__ == '__main__':
-	states = grab_states(gym.make('CartPole-v1'), 100)
-	do_experiment(
-		fname='test',
-		n_steps=5000,
-		mutual_steps=1500,
-		n_trials=20,
-		do_baseline=None,
-		ml_type='both',
-		do_mutual=False,
-		steps_per_eval=100,
-		gamma=0.9
-	)
+	cfg = parse_config('./experiment_configs/exp_target.json')
+	exp = Experiment(cfg)
+
+	exp.run()
