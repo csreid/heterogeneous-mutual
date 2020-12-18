efd8798e88c76d33402cb85a462a656c805ae70f
Cameron Reid <csreid.cr@gmail.com>
2020-12-18 11:27:17 -0500

    Also add a flag for number of processes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from itertools import product
 import pickle
 import time
-from IPython import embed
 import json
 import argparse
 
@@ -25,15 +24,24 @@ parser = argparse.ArgumentParser(
 	description='Run an experiment'
 )
 parser.add_argument('fname')
+parser.add_argument(
+	'--n_jobs',
+	help='Number of trials to run in parallel',
+	default=1,
+	type=int
+)
 
 def parse_config(fname):
 	data = json.load(open(fname, 'r'))
 	return data
 
 if __name__ == '__main__':
-	fname = parser.parse_args().fname
+	args = parser.parse_args()
+	fname = args.fname
+	n_jobs = args.n_jobs
+
 	cfg = parse_config(fname)
 	exp = Experiment(cfg)
 
-	exp.run()
+	exp.run(n_jobs=n_jobs)
 	exp.save(fname)
